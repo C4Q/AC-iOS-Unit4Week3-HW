4b98edca3140d04c00ffac7d64ce5ac536dff785
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2018-01-08 10:21:35 -0500

    programmatic autolayout DONE

--- Unit4Week3Homework-StudentVersion/Views/FavoritesView.swift
+++ Unit4Week3Homework-StudentVersion/Views/FavoritesView.swift
@@ -10,12 +10,45 @@ import UIKit
 
 class FavoritesView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //TODO implement self sizing images
+    
+    lazy var tableView: UITableView = {
+        let tv = UITableView()
+        //create and register a cell
+        tv.register(CityImageTableViewCell.self, forCellReuseIdentifier: "CityImageCell")
+        tv.backgroundColor = .blue
+        return tv
+    }()
+    
+    //Initialization
+    override init(frame: CGRect) {//overriding the parent class's functions
+        super.init(frame: UIScreen.main.bounds)
+        setUpGUI()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) { //now the new initializer required for this uiView
+        super.init(coder: aDecoder)
+        //setUpGUI()
+    }
+    
+    private func setUpGUI() {
+        backgroundColor = .white
+        setupAndConstrainObjects()
+    }
+    
+    private func setupAndConstrainObjects() {
+        
+        //ARRAY MUST BE ON ORDER!!
+        let favoriteObjects = [tableView] as [UIView]
+        
+        favoriteObjects.forEach{addSubview($0); ($0).translatesAutoresizingMaskIntoConstraints = false}
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor)
+            ])
+    }
 
 }
