0e994b6f6cb40ac55933e50ca717a0373a736da0
Xianxian Chen <xianxianchen@ac.c4q.nyc>
2018-01-17 15:37:07 -0500

    add tableView

---
+++ Unit4Week3Homework-StudentVersion/Views/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Unit4Week3Homework-StudentVersion
+//
+//  Created by C4Q on 1/17/18.
+//  Copyright © 2018 C4Q . All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
